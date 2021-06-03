dc0495852e82bb485e2fdd17da24e1f80aa8651a
fguajard <franciscogm31@gmail.com>
2021-06-03 01:43:39 -0400

    inicio del desafio sugerencia de videos, maqueta incluida y agrego maqueta de js

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+class Multimedia{
+    constructor(url){
+        let _url = url
+        this._getUrl = ()=> _url
+        this._setUrl = (url) => _url = url
+    }
+
+    get url() {
+        return this._getUrl()
+    }
+
+    set url(url) {
+         this._setUrl(url)
+    }
+
+    setInicio = () =>{
+        console.log(`Este método es para realizar un cambio en la URL del video`);
+    }
+}
+
+class Reproductor extends Multimedia {
+    constructor(url,id){
+        super(url)
+        this._id = id
+        this._getId = ()=> _id
+        this._setId = (id) => _id = id
+    }
+    get id() {
+        return this._getId()
+    }
+
+    set id(id) {
+         this._setId(id)
+    }
+    
+    setInicio = () =>{
+        //agrega un tiempo al inicio de la URL 
+    }
+    
+    playMultimedia = () =>{
+        //llama a la iife
+    }
+
+}
+
+const iife = (()=>{    
+    const funcionPrivada = (url,id) =>{
+        //mostrar video en HTML
+    }
+    return{
+        funcionPublica : (url,id) => {
+            funcionPrivada(url,id)
+        }
+    }
+})()
